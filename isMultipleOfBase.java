625d650c8936b2e7905b644a08a3db5e02698602
Arman Azarnik <armanazarnik@gmail.com>
2023-09-02 17:21:32 +0330

    v 1.0

---
+++ isMultipleOfBase.java
@@ -0,0 +1,31 @@
+/*Check whether if the input number is a multiple of base or not*/
+
+import java.util.Scanner;
+
+public class isMultipleOfBase{
+    public static void main(String[] args) {
+        while(true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.print("Enter the divisor: ");
+            int divisor = scanner.nextInt();
+
+            if (isMultiple(number, divisor)) {
+                System.out.println(number + " is a multiple of " + divisor + ".");
+            } else {
+                System.out.println(number + " is not a multiple of " + divisor + ".");
+            }
+        }
+    }
+
+    public static boolean isMultiple(int number, int divisor) {
+        if (divisor == 0) {
+            return false; // Avoid division by zero
+        }
+        return number % divisor == 0;
+    }
+}
+
+
+//by Arman Azarnik
